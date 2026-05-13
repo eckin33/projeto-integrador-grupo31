abb5c269044ec6e4e1188d36d49bbcaabf4c5288
Juleli <juliana.lins12@gmail.com>
2026-05-13 19:08:47 -0300

    tarefa das colunas finalizada

--- src/etl.py
+++ src/etl.py
@@ -3,6 +3,24 @@ import pandas as pd
 # Parte de Extract
 df = pd.read_csv('./data/base.csv')
 
+# Traduzir os nomes das colunas para português
+df.rename(columns={
+    'User_ID': 'ID do Usuário',
+    'Age': 'Idade',
+    'Gender': 'Gênero',
+    'Technology_Usage_Hours': 'Horas de Uso de Tecnologia',
+    'Social_Media_Usage_Hours': 'Horas de Uso de Redes Sociais',
+    'Gaming_Hours': 'Horas de Jogos',
+    'Screen_Time_Hours': 'Horas de Tempo na Tela',
+    'Mental_Health_Status': 'Status de Saúde Mental',
+    'Stress_Level': 'Nível de Estresse',
+    'Sleep_Hours': 'Horas de Sono',
+    'Physical_Activity_Hours': 'Horas de Atividade Física',
+    'Support_Systems_Access': 'Acesso a Sistemas de Suporte',
+    'Work_Environment_Impact': 'Impacto do Ambiente de Trabalho',
+    'Online_Support_Usage': 'Uso de Suporte Online'
+}, inplace=True)
+
 print(df.head())
 #print(df.info())
 #print(df.shape())
