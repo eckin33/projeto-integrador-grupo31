31f775d751dcc900e55b61ddb364afd1606e35ea
eckin33 <erickdubisoj4@gmail.com>
2026-05-13 20:08:41 -0300

    Finalização do processo de ETL
    Nesse processo, foram feitas remoções de colunas que não fariam sentido com o avanço do projeto bem como foram adicionadas algumas para futuros gráficos.

--- src/etl.py
+++ src/etl.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 
 # Parte de Extract
 df = pd.read_csv('./data/base.csv')
 
+#Parte de Transform
 # Traduzir os nomes das colunas para português
 df.rename(columns={
     'User_ID': 'ID do Usuário',
@@ -13,7 +15,7 @@ df.rename(columns={
     'Gaming_Hours': 'Horas de Jogos',
     'Screen_Time_Hours': 'Horas de Tempo na Tela',
     'Mental_Health_Status': 'Status de Saúde Mental',
-    'Stress_Level': 'Nível de Estresse',
+    'Stress_Level': 'Nivel_Estresse',
     'Sleep_Hours': 'Horas de Sono',
     'Physical_Activity_Hours': 'Horas de Atividade Física',
     'Support_Systems_Access': 'Acesso a Sistemas de Suporte',
@@ -24,14 +26,28 @@ df.rename(columns={
 # Traduzir os conteúdos das colunas categóricas
 df['Gênero'] = df['Gênero'].map({'Male': 'Masculino', 'Female': 'Feminino', 'Other': 'Outro'})
 df['Status de Saúde Mental'] = df['Status de Saúde Mental'].map({'Good': 'Bom', 'Poor': 'Ruim', 'Fair': 'Regular', 'Excellent': 'Excelente'})
-df['Nível de Estresse'] = df['Nível de Estresse'].map({'Low': 'Baixo', 'Medium': 'Médio', 'High': 'Alto'})
+df['Nivel_Estresse'] = df['Nivel_Estresse'].map({'Low': 'Baixo', 'Medium': 'Médio', 'High': 'Alto'})
 df['Acesso a Sistemas de Suporte'] = df['Acesso a Sistemas de Suporte'].map({'Yes': 'Sim', 'No': 'Não'})
 df['Impacto do Ambiente de Trabalho'] = df['Impacto do Ambiente de Trabalho'].map({'Negative': 'Negativo', 'Positive': 'Positivo', 'Neutral': 'Neutro'})
 df['Uso de Suporte Online'] = df['Uso de Suporte Online'].map({'Yes': 'Sim', 'No': 'Não'})
 
-# Remover a coluna ID do Usuário
+# Remover a coluna ID do usuario
 df.drop('ID do Usuário', axis=1, inplace=True)
 
+#criacao da faixa etaria
+df['Faixa_Etaria'] = pd.cut(df['Idade'], bins=[18, 25, 40, 60, 100], labels=['18-25', '26-40', '41-60', '60+'])
+
+#coluna que o nível de estresse em um valor numérico para facilitar as analises futuras
+mapa_estresse = {'Baixo': 1, 'Médio': 2, 'Alto': 3}
+df['Estresse_Score'] = df['Nivel_Estresse'].map(mapa_estresse)
+
+#coluna que pode indicar um alerta se o usuario tiver poucas horas de sono e nivel alto de horas de tela
+#funciona assim: se o usuario tiver menos de 6 horas de sono e mais de 8 horas de tempo na tela, o alerta sera "sim"
+df['Alerta_Saude'] = np.where((df['Horas de Sono'] < 6) & (df['Horas de Tempo na Tela'] > 8), 'Sim', 'Não')
+
 print(df.head())
-#print(df.info())
-#print(df.shape())
+print(df['Alerta_Saude'].value_counts())
+
+# Parte de Load
+# Após a transformação, salvamos a base tratada em um novo arquivo CSV
+df.to_csv('./data/base_tratada.csv', index=False)
